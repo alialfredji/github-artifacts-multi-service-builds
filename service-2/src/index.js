565ec5884605be30898dfe60e252c52d1e43549a
Ali Alfredji <ali.alfredji@inter.ikea.com>
2025-08-21 16:50:42 +0200

    wp

--- service-2/src/index.js
+++ service-2/src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.json({ service: 'service-2', port });
 });
 
+app.get('/health', (req, res) => {
+    res.json({ service: 'service-2', port, status: 'healthy' });
+  });
+
 app.listen(port, () => {
   console.log(`Service 2 running on port ${port}`);
 });
